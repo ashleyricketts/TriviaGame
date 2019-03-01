f9475a60b25dfc6f05a5befe34f6b0d32f10d195
Ashley Ricketts <aricketts12@gmail.com>
2019-03-01 11:05:57 -0600

    this code does not work I am trying to fix it

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,6 +4,7 @@ var time = 10;
 var count = 0;
 var correct = 0;
 var incorrect = 0;
+var correctAnswer = trivia[count].answer;
 
 var trivia = [
     { 
@@ -19,26 +20,127 @@ var trivia = [
     {
         question: "Phife Dawg, Q-Tip, and Ali Shaheed Muhammad were the members of what rap group?",
         choices: ["De La Soul", "A Tribe Called Quest", "Arrested Development", "Digital Underground"],
-        answers: "A Tribe Called Quest",
+        answer: "A Tribe Called Quest",
     },
     {
         question: "What was the name of Fresh Prince's DJ",
         choices: ["DJ Duke of Earl", "DJ Jazzy Jeff", "DJ Funk Flexmaster", "DJ King Coopa"],
-        answers: "DJ Jazzy Jeff",
+        answer: "DJ Jazzy Jeff",
     }];
 
 
 
 function showQuestions() {
-       $("#question").html("<p>" + trivia[count].question + "</p>" +
+       $("#question").append("<p>" + trivia[count].question + "</p>" +
                             "<p class='choices'>" + trivia[count].choices[0] + "</p>" +
                             "<p class='choices'>" + trivia[count].choices[1] + "</p>" +
                             "<p class='choices'>" + trivia[count].choices[2] + "</p>" +
                             "<p class='choices'>" + trivia[count].choices[3] + "</p>");
 
-};
 
-console.log(showQuestions());
+}
+
+console.log(trivia.question);
+
+
+
+// function win() {
+//     $("#game").html("<p> Correct! </p>");
+//     correct++;
+//     $("#game").append(correctAnswer + trivia[count].image);
+//     setTimeout(nextQuestion, 5000);
+//     count++;
+// }
+
+// function loss() {
+//     $("#game").html("<p> Incorrect! </p>");
+//     incorrect++;
+//     $("#game").append("<p> The answer was </p>" + correctAnswer);
+//     setTimeout(nextQuestion, 5000);
+//     count++;
+
+// }
+
+// function userTimeout() {
+//     if (time === 0) {
+//         $("#game").html("<p> Time's up! </p>");
+//         incorrect++;
+//         $("#game").append("<p> The answer was </p>" + correctAnswer);
+//         setTimeout(nextQuestion, 5000);
+//     count++;
+//     }
+// }
+
+
+// function results() {
+//     $("#game").html("<p> Correct: " + correct + "</p>" + "<p> Incorrect: " + incorrect + "</p>");
+//     gameReset();
+// }
+
+
+// function timer () {
+//     clock = setInterval(decrease, 1000);
+//         function decrease() {
+//             if (time < 1) {
+//                 clearInterval(clock);
+//                 userTimeout();}
+
+//             if (time > 0) {
+//                 time--;}
+
+//             $("#timer").html(time)
+//         }
+// }
+
+// function nextQuestion() {
+//     If (count < trivia.length); {
+//         time = 10;
+//         $("#game").html("<p> You have " + time + "seconds</p>")
+//         showQuestions();
+//         timer();
+//         userTimeout();
+//     }
+
+    // else {
+    //     results();
+    // }
+// }
+
+
+// function gameReset() {
+//     count = 0;
+//     correct = 0;
+//     incorrect = 0;
+// }
+
+
+
+// $("#game").on("click", ".choices", (function(){
+//     var userGuess = $(this).text();
+//         if (userGuess === trivia[count].answer){
+//             clearInterval(clock);
+//             win();
+//         }
+
+//         else {
+//             clearInterval(clock);
+//             loss();
+//         }
+// }))
+
+// $("#start").click(nextQuestion);
+
+// function startGame() {
+//     $("#start").hide();
+//     showQuestions();
+//     timer();
+//     userTimeout();
+
+// }
+    
+
+// }
+
 
 });
 
@@ -46,18 +148,3 @@ console.log(showQuestions());
 
 
 
-
-
-// function timer() {
-//     $("#timer").html(time);
-//     $("#timer").css("visibility", "inherit");
-
-//     if (time === 0) {
-
-//         $(#timer).css("background", "#837362");
-//         clearInterval(intervalID);
-//         time = 10;
-
-
-//     }
-// 
